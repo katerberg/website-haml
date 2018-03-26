d9d0ff3e1d92a2277033f9ff8a9fc51d39075ea3
Mark Katerberg <katerberg@fastmail.fm>
2018-03-25 21:10:09 -0500

    Adds styling

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,26 +2,38 @@
 import gulp from 'gulp';
 import haml from 'gulp-haml';
 import minifyHTML from 'gulp-htmlmin';
+import sass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
 
-const basedir = './';
+const basedir = './src';
+const targetdir = './dist';
 
 // Handle haml and minification of HTML
 gulp.task('html', () => {
-  gulp.src('./haml/**/*.haml')
+  gulp.src(`${basedir}/haml/**/*.haml`)
     .pipe(haml())
     .pipe(minifyHTML({
         "collapseWhitespace": true
     }))
-    .pipe(gulp.dest('./dist/html'));
+    .pipe(gulp.dest(`${targetdir}`));
+});
+
+// Compile Our Sass
+gulp.task('sass', () => {
+    return gulp.src(`${basedir}/scss/**/*.scss`)
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(`${targetdir}/css`));
 });
 
 // Watch Files For Changes
 gulp.task('watch', () => {
-    gulp.watch(`${basedir}**/*.haml`, ['html']);
+    gulp.watch(`${basedir}/**/*.haml`, ['html']);
+    gulp.watch(`${basedir}/**/*.scss`, ['sass']);
 });
 
 // Build task
-gulp.task('build', ['html']);
+gulp.task('build', ['html', 'sass']);
 
 // Default Task
 gulp.task('default', ['build', 'watch']);
